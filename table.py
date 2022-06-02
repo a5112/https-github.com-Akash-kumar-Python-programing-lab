e646c074e5cd2d6a044804da028249cfccc5d27c
a5112 <92076603+a5112@users.noreply.github.com>
2022-06-02 23:18:04 +0530

    Add files via upload

--- table.py
+++ table.py
@@ -1,9 +1,5 @@
-'''Program to display the table of an entered number in the following format:
-2*1=2
-2*2=4
-...........
-2*10=20'''
-
-num = int(input())
-for i in range(1, 11):
-   print(num, 'x', i, '=', num*i)
+def table():
+    x=int(input("enter te number"))
+    for i in range(1,11):
+        print(x,"*",i,"=",x*i)
+table()
